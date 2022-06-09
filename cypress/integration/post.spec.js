b5ec5ceee572df62647ab91e3a0c70c1b96d37e0
mrk-qa <marco.ant.seg@gmail.com>
2022-06-08 23:58:39 -0300

    concluindo desafio QAninja

---
+++ cypress/integration/post.spec.js
@@ -0,0 +1,81 @@
+import Ajv from "ajv"
+const ajv = new Ajv()
+
+describe('POST /characters', function () {
+
+    before(function () {
+        cy.back2ThePast()
+        cy.setToken()
+    })
+
+    it('deve cadastrar um personagem', function () {
+        const character = {
+            name: 'Wanda Maximoff',
+            alias: 'Feiticeira Escarlate',
+            team: ['vingadores'],
+            active: true
+        }
+
+        cy.postCharacter(character)
+            .then(function(response) {
+                expect(response.status).to.eql(201)
+                cy.log(response.body.character_id)
+                expect(response.body.character_id.length).to.eql(24)
+            })
+    })
+
+    context('quando o personagem já existe', function () {
+        const character = {
+            name: 'Pietro Maximoff',
+            alias: 'Mercurio',
+            team: [
+                'vingadores da costa oeste',
+                'irmandade de mutantes'
+            ],
+            active: true
+        }
+
+        before(function () {
+            cy.postCharacter(character)
+                .then(function(response){
+                    expect(response.status).to.eql(201)
+                })
+            })
+        
+        it('não deve cadastrar duplicado', function () {
+            cy.postCharacter(character)
+            .then(function(response){
+                expect(response.status).to.eql(400)
+                expect(response.body.error).to.eql('Duplicate character')
+            })
+        })
+    })
+})
+
+describe('POST /characters', function () {
+    before(function () {
+        cy.back2ThePast()
+        cy.setToken()
+    })
+
+    it('validar campos obrigatórios', function () {
+        const character = {
+            name: 'Pietro Maximoff',
+            alias: 'Mercurio',
+            team: [
+            'vingadores da costa oeste',
+            'irmandade de mutantes'
+            ],
+            active: true
+        }
+    
+        cy.postCharacter(character).then((response) => {
+            cy.fixture('postSchema').then((postSchema) => {
+            const valid = ajv.validate(postSchema)
+            if (!valid) console.log(ajv.errors)
+            cy.log('Campos validados!')
+            expect(response.status).to.eql(201)
+            })
+        })
+    })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,66 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setToken', function () {
+    cy.api({
+        method: 'POST',
+        url: '/sessions',
+        body: {
+            email: 'marco@qacademy.io',
+            password: 'qa-cademy'
+        },
+        failOnStatusCode: false
+    }).then(function (response) {
+        expect(response.status).to.eql(200)
+        Cypress.env('token', response.body.token)
+    })
+})
+
+Cypress.Commands.add('back2ThePast', function () {
+    cy.api({
+        method: 'DELETE',
+        url: '/back2thepast/629ea0b0e78b2a0016d3a098',
+        failOnStatusCode: false
+    }).then(function (response) {
+        expect(response.status).to.eql(200)
+    })
+})
+
+//POST requisição que testa o cadastro de personagens
+Cypress.Commands.add('postCharacter', function(payLoad){
+    cy.api({
+        method: 'POST',
+        url: '/characters',
+        body: payLoad,
+        headers: {
+            Authorization: Cypress.env('token')
+        },
+        failOnStatusCode: false
+    }).then(function(response){
+        return response
+    })
+
+})
